c7a4bbd67dc655b989d7eece9827ae9b58ff116c
mayurkanade44 <mayurkanade44@gmail.com>
2022-05-16 11:32:52 +0530

    intial client setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer position="top-center" />
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default App;
